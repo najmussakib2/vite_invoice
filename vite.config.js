50fb3a25a5992bd9f4d9168fe8fea34c4165b025
najmussakib2 <nnsnajmussakib@gmail.com>
2024-05-24 19:16:32 +0600

    auto reload added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'; // Correct import
+import FullReload from 'vite-plugin-full-reload';
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(), // Use the correct plugin
+    FullReload(['config/routes.rb', 'app/views/**/*']),
+    // Add other plugins as needed
+  ],
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
