33ec4e7855d988e7be398b0f5283295ade1cbf6f
euhansarkar <euhansarkar@gmail.com>
2024-06-27 18:32:56 +0600

    T

--- src/redux/api/invoice/invoiceApi.js
+++ src/redux/api/invoice/invoiceApi.js
@@ -4,8 +4,8 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const invoice = createApi({
   reducerPath: "invoice",
   baseQuery: fetchBaseQuery({
-    // baseUrl: "https://express-invoice.vercel.app/api/v1",
-    baseUrl: "http://localhost:5000/api/v1",
+    baseUrl: "https://express-invoice.vercel.app/api/v1",
+    // baseUrl: "http://localhost:5000/api/v1",
   }),
 
   endpoints: (builder) => ({
